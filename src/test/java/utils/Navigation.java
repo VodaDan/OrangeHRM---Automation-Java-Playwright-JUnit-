b5daf539fec4f9ea12070155aa1de433fd2d1372
Dan <voda.daniel98@yahoo.com>
2025-09-12 13:24:34 +0300

    add navigation class, for direct url navigation

---
+++ src/test/java/utils/Navigation.java
@@ -0,0 +1,23 @@
+package utils;
+import com.microsoft.playwright.*;
+
+public class Navigation {
+
+    private String loginUrl;
+    private String registerUserUrl;
+    private Page page;
+
+    public Navigation (Page testPage) {
+        page = testPage;
+        loginUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+        registerUserUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/admin/saveSystemUser";
+    }
+
+    public void navigateLogin() {
+        page.navigate(loginUrl);
+    }
+
+    public void navigateAddUser() {
+        page.navigate(registerUserUrl);
+    }
+}
