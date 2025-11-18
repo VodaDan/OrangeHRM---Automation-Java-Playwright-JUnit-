ae966b36dca19df72b13098595d89e7a52f2da35
Dan <voda.daniel98@yahoo.com>
2025-11-18 13:19:22 +0200

    add json data provider

--- src/test/java/utils/DataProvider.java
+++ src/test/java/utils/DataProvider.java
@@ -1,11 +1,15 @@
 package utils;
 
 import base.BaseTest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import model.User;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsSource;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -33,5 +37,30 @@ public class DataProvider {
 //        System.out.println("exists?    = " + Files.exists(Paths.get(path)));
 
         return rows.stream().map(r -> Arguments.of(r[0],r[1]));
+    }
+
+    // Data Transfer Object for json parsing
+    public static class RegisterTestDTO extends User {
+        private String expectedPage;
+        public String getExpectedPage() { return expectedPage; }
+    }
+
+    public static Stream<Arguments> registerJsonProvider() throws Exception {
+        Properties properties = new Properties();
+        String path;
+
+        try (InputStream config = BaseTest.class.getClassLoader().getResourceAsStream("config.properties")) {
+            properties.load(config);
+        } catch (IOException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+        path = properties.getProperty("registerJson");
+
+        Gson gson = new Gson();
+        Type listType = new TypeToken<List<RegisterTestDTO>>() {}.getType();
+
+        List<RegisterTestDTO> data = gson.fromJson(new FileReader(Paths.get(path).toFile()), listType);
+
+        return data.stream().map(d -> Arguments.of(d.getUsername(),d.getPassword(),d.getExpectedPage(),d.getFirstName(),d.getMiddleName(),d.getLastName(),d.getEmployeeId()));
     }
 }
